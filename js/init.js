4bfe53a45d03d7f1742b52195a357aa986fd661d
kerry-smith <smithdevco@gmail.com>
2016-06-15 12:25:35 -0700

    fix dropdown to close on click

--- js/init.js
+++ js/init.js
@@ -9,13 +9,9 @@
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
     $('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
       constrain_width: false, // Does not change width of dropdown to that of the activator
       hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: false, // Displays dropdown below the button
-      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+      closeOnClick: true
     }
   );
 
