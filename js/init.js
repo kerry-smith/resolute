ee94a94c54ff746036a52a10d79dba7d45b0fefb
kerry-smith <smithdevco@gmail.com>
2016-06-15 12:41:28 -0700

    update phone link on mobile

--- js/init.js
+++ js/init.js
@@ -11,7 +11,6 @@
     $('.dropdown-button').dropdown({
       constrain_width: false, // Does not change width of dropdown to that of the activator
       hover: true, // Activate on hover
-      closeOnClick: true
     }
   );
 
