84206a83bc7e9ffa2d31329ab43f73fa5705311c
kerry-smith <smithdevco@gmail.com>
2016-06-16 19:59:27 -0700

    Updating site

--- js/init.js
+++ js/init.js
@@ -3,9 +3,7 @@
 
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
-    $('.collapsible').collapsible({
-      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-    });
+    $('.collapsible').collapsible();
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
     $('.dropdown-button').dropdown({
@@ -13,6 +11,7 @@
       hover: true, // Activate on hover
     }
   );
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
