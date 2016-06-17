d10bd3ffa1959c1c845513412ca3d720573af2a5
Kerry Smith <kerry-smith@users.noreply.github.com>
2016-06-16 22:24:41 -0700

    Update init.js

--- js/init.js
+++ js/init.js
@@ -3,7 +3,9 @@
 
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
-    $('.collapsible').collapsible();
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
     $('.dropdown-button').dropdown({
