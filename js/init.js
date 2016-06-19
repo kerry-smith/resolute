9b6411aac03096d47bfe8b81cd5fc7395d7e6460
Kerry Smith <kerry-smith@users.noreply.github.com>
2016-06-18 22:42:17 -0700

    Update init.js

--- js/init.js
+++ js/init.js
@@ -11,6 +11,7 @@
     $('.dropdown-button').dropdown({
       constrain_width: false, // Does not change width of dropdown to that of the activator
       hover: true, // Activate on hover
+      alignment: 'right' // Displays dropdown with edge aligned to the right of button
     }
   );
 
