ef29cb9a84687b1d77aa464fc63c8255af02e7a6
Kerry Smith <kerry-smith@users.noreply.github.com>
2016-06-18 22:27:53 -0700

    Update init.js

--- js/init.js
+++ js/init.js
@@ -9,7 +9,7 @@
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
     $('.dropdown-button').dropdown({
-      constrain_width: false, // Does not change width of dropdown to that of the activator
+      constrain_width: true, // Does not change width of dropdown to that of the activator
       hover: true, // Activate on hover
     }
   );
