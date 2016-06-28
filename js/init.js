0e09a5a77913bfb2f66966e7cfd50c329278cee0
Kerry Smith <kerry-smith@users.noreply.github.com>
2016-06-28 00:55:14 -0700

    Update init.js

--- js/init.js
+++ js/init.js
@@ -18,3 +18,28 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function windowPopup(url, width, height) {
+  // Calculate the position of the popup so
+  // it’s centered on the screen.
+  var left = (screen.width / 2) - (width / 2),
+      top = (screen.height / 2) - (height / 2);
+
+  window.open(
+    url,
+    "",
+    "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=" + width + ",height=" + height + ",top=" + top + ",left=" + left
+  );
+}
+
+// Vanilla JavaScript
+var SharePopup = document.querySelectorAll(".share-popup");
+if (SharePopup) {
+  [].forEach.call(SharePopup, function(anchor) {
+    anchor.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      windowPopup(this.href, 500, 300);
+    });
+  });
+}
