43c2e7bbc03629cbbbf716319c21ee7b1ab82fb0
Kerry Smith <kerry-smith@users.noreply.github.com>
2016-06-28 14:23:26 -0700

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,12 @@
     });
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
-    
-  
-  $('.dropdown-button')dropdown.on('click', function(){
-        $("#menu-dropdown").hide();
-    });
+    $('.dropdown-button').dropdown({
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      alignment: 'right' // Displays dropdown with edge aligned to the right of button
+    }
+  );
 
 
   }); // end of document ready
