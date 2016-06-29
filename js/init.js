85d99af31d031c74bb96e11f9b5286cc83d6cb09
Kerry Smith <kerry-smith@users.noreply.github.com>
2016-06-28 18:06:50 -0700

    Update init.js

--- js/init.js
+++ js/init.js
@@ -10,7 +10,7 @@
     $('.modal-trigger').leanModal();
     $('.dropdown-button').dropdown({
       constrain_width: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
+      hover: false, // Activate on hover
       alignment: 'right' // Displays dropdown with edge aligned to the right of button
     }
   );
