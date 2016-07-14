aa4da2ebaea424443f89f1a833840f2645f143f5
Kerry Smith <kerry-smith@users.noreply.github.com>
2016-07-14 15:58:14 -0700

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,7 +2,7 @@
   $(function(){
 
     $('.parallax').parallax();
-    $('.scrollspy').scrollSpy({ scrollOffsetFix : 55 });
+    $('.scrollspy').scrollSpy();
     $('.collapsible').collapsible({
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
